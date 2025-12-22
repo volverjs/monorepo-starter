1655cb156be35bde9f475bcfec85b5a5f127b93c
Alessandro Bellesia <alessandro.bellesia@24consulting.it>
2025-12-22 12:47:38 +0100

    Dependencies update
    fix: dependencies update
    fix: fastify better-auth integration
    fix: add PjSearchFullText missing component

--- apps/backend/src/auto-imports.d.ts
+++ apps/backend/src/auto-imports.d.ts
@@ -6,12 +6,12 @@
 // biome-ignore lint: disable
 export {}
 declare global {
-  const Container: typeof import('brandi')['Container']
+  const Container: typeof import('brandi').Container
   const db: typeof import('./db/index')['default']
-  const drizzle: typeof import('drizzle-orm')['drizzle']
+  const drizzle: typeof import('drizzle-orm').drizzle
   const entityDefaultColumns: typeof import('./db/defaultColumns')['entityDefaultColumns']
   const entityDefauuserltColumns: typeof import('./db/defaultColumns')['entityDefauuserltColumns']
-  const eq: typeof import('drizzle-orm')['eq']
+  const eq: typeof import('drizzle-orm').eq
   const getFilters: typeof import('./db/helpers')['getFilters']
   const getFullText: typeof import('./db/helpers')['getFullText']
   const getHas: typeof import('./db/helpers')['getHas']
@@ -20,8 +20,8 @@ declare global {
   const getOrderBy: typeof import('./db/helpers')['getOrderBy']
   const getPageNumberAndPageSize: typeof import('./db/helpers')['getPageNumberAndPageSize']
   const getRange: typeof import('./db/helpers')['getRange']
-  const injected: typeof import('brandi')['injected']
+  const injected: typeof import('brandi').injected
   const jsonbArray: typeof import('./db/customTypes')['jsonbArray']
   const querystringToSelect: typeof import('./db/helpers')['querystringToSelect']
-  const token: typeof import('brandi')['token']
+  const token: typeof import('brandi').token
 }
